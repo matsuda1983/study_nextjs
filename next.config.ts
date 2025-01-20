1bd801429e4a559416b905e4bdd96b2ee9761334
松田和浩 <matsuda@e3sys.co.jp>
2025-01-20 20:32:48 +0900

    10章

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,10 @@
 import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  // Chap10
+  // experimental: {
+  //   ppr: 'incremental'
+  // }
 };
 
 export default nextConfig;

79ff1a551c21f2570bc221f758ad47601aab64a0
松田和浩 <matsuda@e3sys.co.jp>
2025-02-10 20:44:04 +0900

    15章-ビルドエラー回避

--- auth.ts
+++ auth.ts
@@ -4,14 +4,18 @@ import Credentials from 'next-auth/providers/credentials';
 import { z } from 'zod';
 import type { User } from '@/app/lib/definitions';
 import bcrypt from 'bcrypt';
-import postgres from 'postgres';
 
-const sql = postgres(process.env.POSTGRES_URL!, { ssl: 'require' });
+import {sql} from '@vercel/postgres'
+// import postgres from 'postgres'
+
+// const sql = postgres(process.env.POSTGRES_URL!, { ssl: 'require' });
 
 async function getUser(email: string): Promise<User | undefined> {
     try {
-      const user = await sql<User[]>`SELECT * FROM users WHERE email=${email}`;
-      return user[0];
+    //   const user = await sql<User[]>`SELECT * FROM users WHERE email=${email}`;
+    //   return user[0];
+    const user = await sql<User>`SELECT * FROM users WHERE email=${email}`;
+    return user.rows[0]
     } catch (error) {
       console.error('Failed to fetch user:', error);
       throw new Error('Failed to fetch user.');
